cdb852472ede03ca97a2ea9a5b152337e4dde742
Abid AAli <coolboy2abid@gmail.com>
2018-06-10 13:24:09 +0500

    updated site

---
+++ tamzidhossain.info/html/jalapino-online/js/test.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    "use strict";
+    
+    $("#testimonial-slider").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,1],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[768,1],
+        margin:10,
+        pagination:false,
+        navigation:true,
+        navigationText:["",""],
+        autoPlay:true
+    });
+});
